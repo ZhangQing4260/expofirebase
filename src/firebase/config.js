92d3c2b82994e45a4cd29247071387258b905a93
ZhangQing4260 <78473641+ZhangQing4260@users.noreply.github.com>
2021-04-28 16:18:00 -0400

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,12 @@ import '@firebase/auth';
 import '@firebase/database';
 
 const firebaseConfig = {
-    apiKey: "Copy Your config from the firebase console"
+    apiKey: "AIzaSyCiyc42phdS3ME1jHJn-nlJ4mLh7pqqZ9w",
+    authDomain: "ux308-final-c5953.firebaseapp.com",
+    projectId: "ux308-final-c5953",
+    storageBucket: "ux308-final-c5953.appspot.com",
+    messagingSenderId: "870282185325",
+    appId: "1:870282185325:web:1599ffa1ca6d45a9184d64"
 };
 
 if (!firebase.apps.length) {
